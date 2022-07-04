b608f7a592ef953dd57298e7b190be2a211c99f8
bytizepearl <106066894+bytizepearl@users.noreply.github.com>
2022-07-04 18:27:56 +0530

    Update schema.lua

--- kong/plugins/http-log-with-body/schema.lua
+++ kong/plugins/http-log-with-body/schema.lua
@@ -10,7 +10,7 @@ return {
           -- NOTE: any field added here must be also included in the handler's get_queue_id method
           { http_endpoint = typedefs.url({ required = true }) },
           { method = { type = "string", default = "POST", one_of = { "POST", "PUT", "PATCH" }, }, },
-          { content_type = { type = "string", default = "application/json", one_of = { "application/json" }, }, },
+          { content_type = { type = "string", default = "application/json", one_of = { "application/json", "text/plain" }, }, },
           { timeout = { type = "number", default = 10000 }, },
           { keepalive = { type = "number", default = 60000 }, },
           { retry_count = { type = "integer", default = 10 }, },
